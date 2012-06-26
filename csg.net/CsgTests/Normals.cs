183ebb55af3a2ac4deb457690160979f28ea49c9
Martin Evans <martindevans@gmail.com>
2012-06-26 16:44:28 +0100

    Added normals test
    Signed-off-by: Martin Evans <martindevans@gmail.com>

---
+++ csg.net/CsgTests/Normals.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Xna.Csg.Primitives;
+using Xna.Csg;
+using Microsoft.Xna.Framework;
+
+namespace CsgTests
+{
+    [TestClass]
+    public class Normals
+    {
+        [TestMethod]
+        public void UpwardNormals()
+        {
+            Polygon p = new Polygon(new[] //Wound anto clockwise
+            {
+                new Vertex(new Vector3(0,0,0), Vector3.Zero),
+                new Vertex(new Vector3(0,0,1), Vector3.Zero),
+                new Vertex(new Vector3(1,0,1), Vector3.Zero),
+                new Vertex(new Vector3(1,0,0), Vector3.Zero),
+            });
+
+            p.CalculateVertexNormals();
+
+            Assert.AreEqual(Vector3.Up, p.Plane.Normal);
+        }
+    }
+}
